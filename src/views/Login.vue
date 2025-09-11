f086473d4349caed23e762becdb08b017417d81e
Seungjin Lee <dltmdwls154@gmail.com>
2025-09-11 23:17:39 +0900

    test

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,15 +1,20 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+
+interface A {
+  name: string
+  password: string
+} 
+
+const a = ref<A>({
+  name: '',
+  password: '',
+})
+
+</script>
+
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+<div></div>
 </template>
 
-<style scoped>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
-}
-</style>
