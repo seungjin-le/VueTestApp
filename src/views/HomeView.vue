79fe71bf56349d98436e4547d61da33150dcc0e9
Seungjin Lee <dltmdwls154@gmail.com>
2025-09-17 21:59:46 +0900

    test

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,58 +1,38 @@
 <script setup lang="ts">
 import { formatErrorsToZodFormattedError, useForm, type Field } from 'formango';
-import { computed, ref, watch } from 'vue';
+import { computed, ref, watch, watchEffect } from 'vue';
 import z, { type ZodFormattedError } from 'zod';
 
-const { register, submit ,state} = useForm({
-  schema: z.object({
-    email: z.string().min(1),
-    password: z.string().min(1),
-  }),
-  initialState: {
-    email: '',
-    password: '',
-  },
-  onSubmit: (data) => {
-    console.log(data)
-  }
+
+const test2 = z.object({
+  email: z.string().min(1),
+  password: z.string().min(1),
+})
+
+const { register, submit ,state} = useForm({schema:test2,initialState:{ email: '', password: ''},onSubmit: (data) => {
+  console.log(data)
+}
 })
 
 
 
 
-function toFormField<TValue, TDefaultValue>(field: Field<TValue, TDefaultValue>): {
-  'isTouched': boolean | undefined
-  'errors': ZodFormattedError<TValue>
-  'modelValue': TDefaultValue extends undefined ? TValue | null : TValue
-  'onBlur': () => void
-  'onUpdate:modelValue': (value: TValue | null) => void
-} {
-  return {
-    'isTouched': field.isTouched.value,
-    'errors': formatErrorsToZodFormattedError(field.errors.value),
-    'modelValue': field.modelValue.value,
-    'onBlur': field.onBlur,
-    'onUpdate:modelValue': field['onUpdate:modelValue'],
-  }
-}
 
-const test = computed(() => register('email').modelValue.value)
-console.log(register('email').modelValue)
 
-watch(() => register('email').modelValue.value, (newVal) => {
-  console.log(newVal)
-}, { immediate: true, deep: true })
+watchEffect(() => {
+  console.log(state)
+
+})
 </script>
 
 <template>
 <div>
 
 <div class="text-white">
-  {{ test }}dd
 </div>
-  <input v-bind="toFormField(register('email'))" true-value="네"
+  <input v-bind="register('email').value" true-value="네"
   false-value="아니오"/>
-  <input v-bind="toFormField(register('password'))" true-value="네"
+  <input v-bind="register('password').value" true-value="네"
   false-value="아니오"/>
   <button @click="submit">Submit</button>
 </div>
