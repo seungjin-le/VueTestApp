7b0535fe4a9d420793e8686c4a043269d7e07f75
bibibig <dltmdwls154@gmail.com>
2026-01-18 23:52:36 +0900

    Formango Test

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -2,25 +2,23 @@
 import { useForm } from 'formango';
 import z from 'zod';
 
-const { register, submit } = useForm({
-  schema: z.object({
-    email: z.string().min(1),
-    password: z.string().min(1),
-  }),
-  initialState: {
-    email: '',
-    password: '',
-  },
-  onSubmit: async (data) => {
-    console.log(data);
-  },
-});
+const schema = z.object({
+  email: z.string().min(1),
+  password: z.string().min(1),
+}); 
+
+type Schema = z.infer<typeof schema>;
+
+const { register, submit } = useForm<Schema>({ schema });
+
+const email = register('email');
+const password = register('password');
 </script>
 
 <template>
   <div>
-    <input type="text" v-bind="register('email')" />
-    <input type="text" v-bind="register('password')" />
-    <button :click="submit">Test</button>
+    <input type="text" v-bind="email" />
+    <input type="text" v-bind="password" />
+    <button @click="submit">Test</button>
   </div>
 </template>
